0a93c073c3795d0193326f72bdee258141514e58
Pablo <pablocasale83@gmail.com>
2019-10-07 20:11:27 -0300

    Add project files.

---
+++ GeekTrade/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeekTrade
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var products = GetProducts();
+            if (products.Count > 0)
+            {
+                Listviewproducts.ItemsSource = products;
+            }
+
+        }
+
+        private List<Product> GetProducts()
+        {
+            var p = new List<Product>();
+            for (int i = 0; i < 20; i++)
+            {
+                p.Add(new Product($"Prod {i}", 784.56, "/Img/Icons/dk.jpg"));
+            }
+            return p;
+        }
+
+        private void Btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+            ScreenNews.Visibility = Visibility.Hidden;
+            ScreenListView.Visibility = Visibility.Visible;
+        }
+    }
+}
